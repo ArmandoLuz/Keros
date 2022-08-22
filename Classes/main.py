4a920d59404ce9ec73876013641f9f57baed69e1
ArmandoLuz <armandoari.288@gmail.com>
2022-08-21 22:09:49 -0300

    src

--- Classes/main.py
+++ Classes/main.py
@@ -1,36 +1,45 @@
 from model import MClassifier
 from rich import print
 import numpy as np
+from model_tools import save, load
 
 #Teste simples com o operador XOR
 x_train = np.array([[0, 0], 
-              [0, 1], 
-              [1, 0], 
-              [1, 1]])
+                    [0, 1], 
+                    [1, 0], 
+                    [1, 1]])
 
 y = np.array([[0], [1], [1], [0]])
 
-x_test = np.array([[0, 0], 
+x_test = np.array([[0, 1], 
                    [1, 0]])
 
-#Instanciando a classificador
-m = MClassifier()
+#Instanciando a classificador (Modelo já foi treinado e saldo)
+#m = MClassifier()
 
-#Treinando o modelo
-r = m.fit(x_train, y, epoch=5000, neurons=1000, learning_rate=0.1, error_threshold=0.05, moment=1)
+#Treinando o modelo (Já foi treinado)
+#r = m.fit(x_train, y, epoch=5000, neurons=1000, learning_rate=0.1, error_threshold=0.05, moment=1)
 
-#Metrics
-print("Epochs: {}".format(r))
-print("Accuraccy: {}".format(m._accuracy))
-print("Precision: {}".format(m._precision))
-print("Recall: {}".format(m._recall))
-print("F1: {}".format(m._f1))
-print("Kappa: {}".format(m._kappa))
+#Salvando o modelo (Já foi salvo)
+#save(m, "model.pkl")
+
+#Carregando o modelo
+loaded_model = load("model.pkl")
 
 #Testando o modelo
-test = m.predict(x_test)
+test = loaded_model.predict(x_test)
+print("-----------------Teste de predição-----------------")
+print("Predição do teste [[0, 0], [0, 1]]\nRetorno: {}".format(test))
+print("---------------------------------------------------")
 
-print("Predição do teste [0, 0]: {}".format(test))
+#Metrics
+print("---------------------Metrics-----------------------")
+print("Accuraccy: {}".format(loaded_model._accuracy))
+print("Precision: {}".format(loaded_model._precision))
+print("Recall: {}".format(loaded_model._recall))
+print("F1: {}".format(loaded_model._f1))
+print("Kappa: {}".format(loaded_model._kappa))
+print("---------------------------------------------------")
 
 
 
