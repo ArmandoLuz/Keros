dd5f6e28faa467980ffcb1ffca0a7282c5685c53
ArmandoLuz <armandoari.288@gmail.com>
2022-12-21 16:15:54 -0300

    Classe que contém ferramentas que podem ser usadas no modelo.

--- Classes/model_tools.py
+++ Classes/model_tools.py
@@ -3,16 +3,34 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 class Tools:
 
-    """Esta classe tem como objetivo facilitar a criação de modelos e a avaliação deles."""
+    """Esta classe tem como objetivo facilitar a criação de modelos e a avaliação deles.
+    
+    Esta classe permite salvar e carregar modelos, além de calcular métricas de avaliação.
+
+    Methods
+    -------
+    metrics(y_true, y_pred)
+        Calcula as métricas de avaliação.
+    save(model, filename)
+        Salva um modelo em um arquivo.
+    load(filename)
+        Carrega um modelo de um arquivo.
+    """
 
     @staticmethod
     def metrics(y_true, y_pred):
-        """
-        Info:
-            Calcula as métricas de acurácia, precisão, recall, f1 e kappa.
-        Params:
-            y_true (array numpy): Valores corretamente marcados.
-            y_pred (array numpy): Valores preditos.
+        """Calcula as métricas de acurácia, precisão, recall, f1 e kappa.
+
+        Parameters
+        ----------
+        y_true : Array numpy
+            Array com os valores reais.
+        y_pred : Array numpy
+            Array com os valores preditos.
+        
+        Returns
+        -------
+            Retorna as métricas de avaliação.
         """
         accuracy = accuracy_score(y_true, y_pred)
         precision = precision_score(y_true, y_pred)
@@ -24,23 +42,26 @@ class Tools:
 
     @staticmethod
     def save(model, filename):
+        """Salva o modelo em um arquivo pkl.
+
+        Parameters
+        ----------
+        model : Objeto model
+            Rede neural.
+        filename : str
+            path do arquivo.
         """
-        Info:
-            Salva o modelo em um arquivo pkl.
-        Params:
-            model (object model): Modelo a ser salvo.
-            filename (string): Nome do modelo.
-        """
-        with open("../models/" + filename, 'wb') as f:
+        with open(filename, 'wb') as f:
             pickle.dump(model, f)
 
     @staticmethod
     def load(filename):
+        """Carrega o modelo salvo.
+
+        Parameters
+        ----------
+        filename : str 
+            Path com o caminho para o modelo.
         """
-        Info:
-            Carrega o modelo salvo.
-        Params:
-            filename (string): Nome do modelo.
-        """
-        with open("../models/" + filename, 'rb') as f:
+        with open(filename, 'rb') as f:
             return pickle.load(f)
